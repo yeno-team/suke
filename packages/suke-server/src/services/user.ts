0686da11f21820e3f8eea1de899857dfbe263d8a
khai93 <33293519+khai93@users.noreply.github.com>
2021-09-23 04:33:47 -0500

    add createUser

--- packages/suke-server/src/services/user.ts
+++ packages/suke-server/src/services/user.ts
@@ -1,6 +1,6 @@
 import { Inject, Service } from 'typedi'
 import { InjectRepository } from 'typeorm-typedi-extensions'
-import { UserModel } from '@suke/suke-core/src/entities/User'
+import { User, UserModel } from '@suke/suke-core/src/entities/User'
 import { Repository } from 'typeorm'
 
 
@@ -12,5 +12,18 @@ export class UserService {
 
     public async findUserById(id: number): Promise<UserModel> {
         return (await this.userRepository.findByIds([id]))[0];
+    }
+
+    public async createUser(user: User): Promise<UserModel> {
+        const newUser = new UserModel();
+        newUser.email = user.email;
+        newUser.id = user.id;
+        newUser.name = user.name;
+        newUser.password = user.password;
+        newUser.role = user.role;
+        newUser.salt = user.salt;
+        
+        
+        return newUser.save();
     }
 }
