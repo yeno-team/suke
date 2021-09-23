8113f925663aeb7064c05443c6665df728263828
khai93 <33293519+khai93@users.noreply.github.com>
2021-09-23 04:33:02 -0500

    add protected IsValid method

--- packages/suke-core/src/ValueObject.ts
+++ packages/suke-core/src/ValueObject.ts
@@ -1,5 +1,10 @@
 export abstract class ValueObject {
     protected abstract GetEqualityProperties(): Generator<any>;
+    /**
+     * IsValid should be called in the constructor and should check the types of primitive values.
+     * Objects passed in could come in from express request body object which is unknown.
+     */
+    protected abstract IsValid(): boolean;
 
     public Equals(obj: unknown): boolean {
         if (!(obj instanceof ValueObject)) return false;
