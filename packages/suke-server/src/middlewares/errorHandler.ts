cf28bd5ffc349d66ff29621cc7c5c543d11e7f7e
khai93 <33293519+khai93@users.noreply.github.com>
2021-09-23 04:33:34 -0500

    show detail if it is a query error

--- packages/suke-server/src/middlewares/errorHandler.ts
+++ packages/suke-server/src/middlewares/errorHandler.ts
@@ -4,6 +4,6 @@ export const ErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     console.error(err);
 
     return res.status(500).send({
-        error: err.message.toString()
+        error: err.detail != null ? err.detail.toString() : err.message.toString()
     });
 }
