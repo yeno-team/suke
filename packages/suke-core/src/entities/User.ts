8511803ba674a625046853ca8abee84c29b4badb
khai93 <33293519+khai93@users.noreply.github.com>
2021-09-23 04:32:33 -0500

    add Validation, made usernames unique

--- packages/suke-core/src/entities/User.ts
+++ packages/suke-core/src/entities/User.ts
@@ -1,8 +1,9 @@
-import { Column, Entity, Index, ObjectIdColumn, PrimaryGeneratedColumn } from "typeorm";
+import { BaseEntity, Column, Entity, Index, ObjectIdColumn, PrimaryGeneratedColumn } from "typeorm";
 import { Role } from '../Role';
 import { ValueObject } from '../ValueObject';
-import { lowercaseTransformer } from './ValueTransformers';
-
+import { lowercaseTransformer } from '../transformers/ValueTransformers';
+import { PropertyValidationError, ValidationError } from "../exceptions/ValidationError";
+import { isValidEmail } from '@suke/suke-util';
 export interface IUser {
     id: number;
     name: string;
@@ -29,6 +30,10 @@ export class User extends ValueObject implements IUser {
         this.password = user.password;
         this.salt = user.salt;
         this.role = user.role;
+
+        if (!this.IsValid()) {
+            throw new ValidationError(`User object ${JSON.stringify(user)} is not valid`);
+        }
     }
 
     protected *GetEqualityProperties(): Generator<any, any, unknown> {
@@ -39,14 +44,59 @@ export class User extends ValueObject implements IUser {
         yield this.salt;
         yield this.role;
     }
+
+    protected IsValid(): boolean {
+        if (this.id == null) {
+            this.id = -1;
+        }
+
+        if (this.salt == null) {
+            this.salt = "";
+        }
+
+        if (this.role == null) {
+            this.role = Role.User;
+        }
+
+        if (typeof(this.id) !== 'number') {
+            throw new PropertyValidationError('id');
+        }
+
+        if (typeof(this.email) !== 'string' || !isValidEmail(this.email)) {
+            throw new PropertyValidationError('email');
+        }
+
+        if (typeof(this.name) !== 'string') {
+            throw new PropertyValidationError('name');
+        }
+
+        if (typeof(this.password) !== 'string') {
+            throw new PropertyValidationError('password');
+        }
+
+        if (typeof(this.salt) !== 'string') {
+            throw new PropertyValidationError('salt');
+        }
+
+        if (typeof(this.role) !== 'number') {
+            throw new PropertyValidationError('role');
+        }
+
+        return true;
+    }
 }
 
 @Entity()
-export class UserModel implements IUser {
-    @ObjectIdColumn()
+export class UserModel extends BaseEntity implements IUser  {
+    @PrimaryGeneratedColumn()
     public id!: number;
 
-    @Column()
+    @Index({ unique: true })
+    @Column({
+        unique: true,
+        nullable: false,
+        transformer: [lowercaseTransformer],
+    })
     public name!: string;
 
     @Index({ unique: true })
