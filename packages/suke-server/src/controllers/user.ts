546697d1d9c5efc36c06e0c15aeee514e8cb7b9d
khai93 <33293519+khai93@users.noreply.github.com>
2021-09-23 04:34:12 -0500

    add Post method to create user

--- packages/suke-server/src/controllers/user.ts
+++ packages/suke-server/src/controllers/user.ts
@@ -2,10 +2,11 @@ import { Service } from "typedi";
 import { UserService } from "../services/user";
 import { BaseController } from "./BaseController";
 import { Express, Request, Response } from 'express';
+import { User } from "@suke/suke-core/src/entities/User";
 
 @Service()
 export class UserController extends BaseController {
-    public route: string = "/api/user/:id";
+    public route: string = "/api/user/:id?";
 
     constructor(
         private userService: UserService
@@ -13,11 +14,25 @@ export class UserController extends BaseController {
         super();
     }
 
-    public async Get(req: Request, res: Response) {
+    public Get = async (req: Request, res: Response) => {
         const id = req.params.id;
 
         const user = this.userService.findUserById(parseInt(id));
 
         res.send(user);
     }
+
+    public Post = async (req: Request, res: Response) => {
+        const userObj = new User(req.body);
+
+        const createdUser = await this.userService.createUser(userObj);
+
+        res.status(200).send({
+            message: 'Created',
+            user: {
+                ...createdUser
+            }
+        });
+    }
+
 }
