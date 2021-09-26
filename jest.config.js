dd92eebb72dd222bb5426d69b2a592b7bc4adc5f
khai93 <33293519+khai93@users.noreply.github.com>
2021-09-26 00:40:16 -0500

    fix global test by seperating react

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    preset: 'ts-jest',
+    transform: {
+      '^.+\\.(ts|tsx)?$': 'ts-jest',
+      "^.+\\.(js|jsx)$": "babel-jest",
+    },
+    moduleNameMapper: {
+        '\\.(css|less)$': '<rootDir>/test/__mocks__/styleMock.js',
+    },
+    modulePathIgnorePatterns: ["<rootDir>/packages/suke-web"]
+};
