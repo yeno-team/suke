5237255f797a6aa8759851baa856a077d1c57002
Luna <bedgowns@gmail.com>
2021-10-02 21:16:54 -0700

    kickassanime searchForAnime method + test

--- packages/suke-kickassanime/src/tests/KickAssAnime.test.ts
+++ packages/suke-kickassanime/src/tests/KickAssAnime.test.ts
@@ -2,12 +2,14 @@ import "reflect-metadata";
 import axios from "axios";
 import { Container } from "typedi";
 import KickAssAnime , { KickAssAnimeLink } from "..";
-import { mockBadEpisodeResponses , mockGoodEpisodeResponses } from "./mocks/mockEpisodesResponses"
+import { mockBadEpisodeResponses , mockGoodEpisodeResponses } from "./mocks/mockEpisodesResponses";
+import { mockAnimeSearchResponses } from "./mocks/mockAnimeSearchResponses";
 
 jest.mock("axios")
 afterEach(() => jest.resetAllMocks())
 
 const mockAxios = axios as jest.Mocked<typeof axios>
+const link = new KickAssAnimeLink("https://www2.kickassanime.ro/anime/khai-69420")
 
 describe("KickAssAnime class" , () => {
     const kickAssAnime = Container.get(KickAssAnime)
@@ -16,23 +18,29 @@ describe("KickAssAnime class" , () => {
         test("should return an array of anime episodes" , async () => {
             for(const mockResponse of mockGoodEpisodeResponses) {
                 mockAxios.get.mockResolvedValue({ data : mockResponse })
-                
-                expect(Array.isArray(await kickAssAnime.getEpisodes(new KickAssAnimeLink("https://www2.kickassanime.ro/anime/khai-69420")))).toBeTruthy()
+                expect(Array.isArray(await kickAssAnime.getEpisodes(link))).toBeTruthy()
             }
         })
 
         test("should not return an array of anime episodes" , async () => {
             for(const mockResponse of mockBadEpisodeResponses) {
                 mockAxios.get.mockResolvedValueOnce({ data : mockResponse })
-
-                expect(await kickAssAnime.getEpisodes(new KickAssAnimeLink("https://www2.kickassanime.ro/anime/khai-763634"))).toBe(null)
+                expect(await kickAssAnime.getEpisodes(link)).toBe(null)
             }
         })
     })
 
-    // describe("searchForAnime()" , () => {
-    //     test("should fetch an array of animes that matches the keyword" , async () => {
-            
-    //     })
-    // })
+    describe("searchForAnime()" , () => {
+        test("should fetch an array of animes that matches the keyword", async() => {
+            for(const mockResponse of mockAnimeSearchResponses) {
+                mockAxios.post.mockResolvedValue({ data : mockResponse })
+                expect(Array.isArray(await kickAssAnime.searchForAnime("test"))).toBeTruthy()
+            }
+        })
+
+        test("should return null when searching for animes" , async () => {
+            mockAxios.post.mockResolvedValue({ data : null })
+            expect(await kickAssAnime.searchForAnime("test")).toBe(null)
+        })
+    })
 })
