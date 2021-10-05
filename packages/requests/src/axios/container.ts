307f7e0aa359606e1a977f671db781190f56363c
Luna <bedgowns@gmail.com>
2021-10-04 20:51:35 -0700

    requests package

---
+++ packages/requests/src/axios/container.ts
@@ -0,0 +1,6 @@
+import "reflect-metadata";
+import { Container , Token } from "typedi"
+import axios from "axios"
+
+export const AxiosInstanceToken = new Token<string>("AxiosInstance")
+Container.set(AxiosInstanceToken , axios.create({ timeout : 5000 }))
---
+++ packages/requests/src/axios/index.ts
@@ -0,0 +1,50 @@
+import { AxiosInstanceToken } from "./container"
+import { Service , Inject } from "typedi";
+import { IRequest, RequestOptions } from "../IRequest";
+import { AxiosInstance, AxiosProxyConfig, AxiosRequestConfig, Method } from "axios";
+
+@Service()
+export class AxiosRequest implements IRequest {
+    constructor(
+        @Inject(AxiosInstanceToken) public axiosInstance : AxiosInstance 
+    ) {}
+
+    private convertToAxiosOpts(options : RequestOptions) : AxiosRequestConfig {
+        const axiosClientOpts: AxiosRequestConfig = {
+            url: options.url,
+            headers : options.headers,
+            method: options.method as Method,
+            baseURL: options.baseURL,
+            params: options.params,
+            data: options.body,
+            withCredentials: options.CORS,
+            maxRedirects: options.maxRedirects,
+            httpAgent: options.httpAgent,
+            httpsAgent: options.httpsAgent,
+            proxy: options.proxy as unknown as AxiosProxyConfig
+        }
+
+        return axiosClientOpts;
+    }
+
+    async request<R>(options: RequestOptions):  Promise<R> {
+        const req = await this.axiosInstance.request(this.convertToAxiosOpts(options))
+        return req.data
+    }
+
+    async get<R>(url: string, options?: RequestOptions): Promise<R> {
+        return await this.request({
+            url,
+            method : "GET",
+            ...options
+        })
+    }
+
+    async post<R>(url: string, options?: RequestOptions): Promise<R> {
+        return await this.request({
+            url,
+            method : "POST",
+            ...options
+        })
+    }
+}
