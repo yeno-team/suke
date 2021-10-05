46b76c7c3b0f6614d7018d3874614d4f8a3cc508
Luna <bedgowns@gmail.com>
2021-10-04 17:22:00 -0700

    fixed IParser class

--- packages/parsers/src/kickassanime/index.ts
+++ packages/parsers/src/kickassanime/index.ts
@@ -1,6 +1,17 @@
 import { ISearchResult } from "packages/suke-core/src/entities/SearchResult";
 import { IParser, ParserSearchOptions } from "../IParser";
 
+export enum KickAssAnimeServers {
+    "PINK-BIRD",
+    "SAPPHIRE-DUCK",
+    "MAGENTA13",
+    "A-KICKASSANIME",
+    "THETA-ORIGINAL",
+    "BETAPLAYER",
+    "BETASERVER3",
+    "BETA-SERVER"
+}
+
 
 export class KickAssAnimeParser implements IParser {
     search(searchTerm: string, options: ParserSearchOptions): ISearchResult {
