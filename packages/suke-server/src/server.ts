47998b2ae8812af9abbce04e9286761680a3424b
khai93 <33293519+khai93@users.noreply.github.com>
2021-10-06 23:31:06 -0500

    override express types

--- packages/suke-server/src/server.ts
+++ packages/suke-server/src/server.ts
@@ -1,30 +1,49 @@
-import express from "express";
+import express, { RequestHandler } from "express";
 import { IConfiguration } from "./config/Configuration";
 import cors from 'cors';
 import { ErrorHandler } from "./middlewares/errorHandler";
 import { Container } from "typeorm-typedi-extensions";
-import { UserController } from "./controllers/user";
-import { UserChannelController } from "./controllers/channel";
+import controllers from './controllers';
 import { SocketServer } from "@suke/suke-socket-server/src/server";
 import session from 'express-session';
 import http from 'http';
+import { IUser, UserModel } from "@suke/suke-core/src/entities/User";
+
+interface ExpressLocals {
+    user?: UserModel;
+}
+
+declare module 'express-session' {
+    interface SessionData {
+      user: IUser;
+    }
+}
+
+declare module 'express' {
+    interface Response {
+        locals: ExpressLocals;
+    }
+}
+
 export class Server {
     private app;
     private sockerServer: SocketServer;
     private server: http.Server;
+    private sessionParser: RequestHandler;
 
     constructor(private config: IConfiguration) {
+
         this.app = express();
         this.server = http.createServer(this.app);
-        this.sockerServer = new SocketServer(this.server, session(config.session));
+        this.sessionParser = session(config.session);
+        this.sockerServer = new SocketServer(this.server, this.sessionParser);
     }
 
     private setupControllers(): void {
-        const userController = Container.get(UserController);
-        userController.execute(this.app);
-
-        const userChannelController = Container.get(UserChannelController);
-        userChannelController.execute(this.app);
+        controllers.map((controller) => {
+            const c = Container.get(controller);
+            c.execute(this.app);
+        });
     }
 
 
@@ -32,6 +51,7 @@ export class Server {
         this.app.use(express.json());
         this.app.use(express.urlencoded({ extended: true }));
         this.app.use(cors({origin: "*"}));
+        this.app.use(this.sessionParser);
 
         this.setupControllers();
 
