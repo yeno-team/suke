dc3c16d41bef89c6a690444824bfe12f2bf09b4d
khai93 <33293519+khai93@users.noreply.github.com>
2021-10-06 21:02:03 -0500

    Add SocketServer

--- packages/suke-socket-server/src/server.ts
+++ packages/suke-socket-server/src/server.ts
@@ -1,19 +1,69 @@
 import WebSocket from 'ws';
+import EventEmitter from "events"
 import { Server, IncomingMessage} from 'http'
-
 import { RequestHandler, Request, Response} from 'express';
-import { IUser } from '@suke/suke-core/dist/entities/User';
+import { UserId } from '@suke/suke-core/src/entities/UserId';
+import TypedEmitter from "typed-emitter";
+import { ValueObject } from '@suke/suke-core/src/ValueObject';
+import { ValueObjectMap } from '@suke/suke-core/src/ValueObjectMap';
+import { ValidationError } from '@suke/suke-core/src/exceptions/ValidationError';
+import { IHasUserId } from 'packages/suke-core/src/entities/UserId/UserId';
+import { createTestHandler } from './handlers';
 
-export class SocketServer {
+
+export interface SocketServerMessage {
+    type: string;
+    data: unknown;
+}
+
+export class SocketServerMessage extends ValueObject implements SocketServerMessage {
+    public type: string;
+    public data: unknown;
+
+    constructor(msg: SocketServerMessage) {
+        super();
+        this.type = msg.type;
+        this.data = msg.data;
+
+        if (this.IsValid()) {
+            throw new ValidationError(`object ${JSON.stringify(msg)} is not a valid socket server message`);
+        }
+    }
+
+    protected *GetEqualityProperties(): Generator<unknown, unknown, unknown> {
+        yield this.type;
+        yield this.data;
+        return;
+    }
+
+    protected IsValid(): boolean {
+        return typeof(this.type) == 'string' && this.type != null;
+    }
+}
+
+export interface SocketServerEvents {
+    error: (error: Error) => void,
+    message: (json: SocketServerMessage) => void
+}
+
+/**
+ * This type extends the interface IHasUserId to the session property. This allows usage of userId which is eventually passed in from express.
+ * It also extends Request from ws and IncomingMessage to allow usage of those.
+ */
+type EventRequest = Request & IncomingMessage & {session: IHasUserId};
+
+export class SocketServer extends(EventEmitter as new () => TypedEmitter<SocketServerEvents>) {
+    private server: Server;
     private wss: WebSocket.Server;
-    private users: Map<number, WebSocket>;
+    private userMap: ValueObjectMap<UserId, WebSocket>;
 
     constructor(httpServer: Server, sessionParser: RequestHandler) {
+        super();
         const wss = new WebSocket.Server({ clientTracking: false, noServer: true });
 
-        httpServer.on('upgrade', (req: Request & IncomingMessage, socket, head) => {
+        httpServer.on('upgrade', (req: EventRequest, socket, head) => {
             sessionParser(req, {} as Response, () => {
-                if (!req.session.id) {
+                if (!req.session.userId) {
                     socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n');
                     socket.destroy();
                     return;
@@ -26,15 +76,46 @@ export class SocketServer {
             });
         });
 
+        this.server = httpServer;
         this.wss = wss;
-        this.users = new Map();
+        this.userMap = new Map();
+
+        this.setupListeners();
     }
 
     private setupListeners(): void {
-        this.wss.on('connection', (ws, req: Request & IncomingMessage) => {
-            const userId =  req.session.id;
+        this.wss.on('connection', (ws, req: EventRequest) => {
+            try {
+                const userId = req.session.userId;
     
-            
+                this.userMap.set(userId, ws);
+
+                ws.on('message', (message) => {
+                    const msgJson = JSON.parse(message.toString());
+
+                    this.emit('message', new SocketServerMessage(msgJson));
+                });
+
+                ws.on('close', () => {
+                    this.userMap.delete(userId);
+                });
+
+                this.createHandlers();
+            } catch (e) {
+                this.emit('error', e as Error);
+            }
         });
+    }
+
+    private createHandlers(): void {
+        createTestHandler(this);
+    }
+
+    public getUserConnection(id: UserId): void {
+        this.userMap.get(id);
+    }
+
+    public start(port: number, cb: () => void): void {
+        this.server.listen(port, cb);
     }
 }
