55413760c42b66764c2b9df7ea5fe84f21d8c486
khai93 <33293519+khai93@users.noreply.github.com>
2021-10-06 23:31:26 -0500

    add Auth Controller

---
+++ packages/suke-server/src/controllers/auth.ts
@@ -0,0 +1,36 @@
+import { Express, Request, Response } from "express";
+import { UserIdentifier } from "@suke/suke-core/src/entities/User/User";
+import { Service } from "typedi";
+import { createUserAttacher } from "../middlewares/createUserAttacher";
+import { UserService } from "../services/user";
+import { BaseController } from "./BaseController";
+import { catchErrorAsync } from "../middlewares/catchErrorAsync";
+
+@Service()
+export class AuthController extends BaseController {
+    public route = "/api/auth";
+
+    constructor(
+        private userService: UserService
+    ) {
+        super();
+    }
+
+    public execute(app: Express): void {
+        app.route(this.route)
+            .post(createUserAttacher(UserIdentifier.Username), catchErrorAsync(this.Post))
+    }
+
+    public Post = async (req: Request, res: Response): Promise<void> => {
+        const password = req.body.password;
+        
+        const check = await res.locals.user?.testRawPassword(password);
+
+        if (check) {
+            req.session.user = res.locals.user;
+            res.send("Authenticated!");
+        } else {
+            res.send("BAD");
+        }
+    }
+}
