6042a1d331b0ff00bc6bb02146442cf258abb6b7
khai93 <33293519+khai93@users.noreply.github.com>
2021-10-13 05:38:56 -0500

    add isValidJson util function

---
+++ packages/suke-util/src/isValidJson/index.ts
@@ -0,0 +1,13 @@
+/* eslint-disable no-useless-escape */
+
+/**
+ * Tests if a string is a valid json,
+ * from: https://github.com/prototypejs/prototype/blob/560bb59414fc9343ce85429b91b1e1b82fdc6812/src/prototype/lang/string.js#L699
+ */
+export default (str: string): boolean => {
+    if (str == "") return false;
+    str = str.replace(/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g, '@');
+    str = str.replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']');
+    str = str.replace(/(?:^|:|,)(?:\s*\[)+/g, '');
+    return (/^[\],:{}\s]*$/).test(str);
+}
