4e69c56fb8c53ef1a42cf91fbb7242c1302167db
Luna <bedgowns@gmail.com>
2021-10-17 16:43:45 -0700

    able to get vid files for old and new vid player

--- packages/parsers/src/index.ts
+++ packages/parsers/src/index.ts
@@ -3,7 +3,7 @@ import { Container } from "typedi";
 import { KickAssAnimeParser } from "./kickassanime";
 
 const a = Container.get(KickAssAnimeParser)
-a.getVideos("https://www2.kickassanime.ro/anime/megaton-kyuu-musashi-206389/episode-01-798828").then(console.log)
+a.getVideos("https://www2.kickassanime.ro/anime/kingdom-3rd-season-853741/episode-26-467503").then(console.log).catch(console.error)
 
 export default [
     KickAssAnimeParser
