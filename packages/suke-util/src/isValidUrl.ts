72163f470422d1b9d41bfd86f2ef2e412eb7e8a5
Luna <bedgowns@gmail.com>
2021-10-18 20:50:52 -0700

    isValidUrl util function

---
+++ packages/suke-util/src/isValidUrl.ts
@@ -0,0 +1,4 @@
+export default function isValidUrl(str : string) : boolean {
+    const urlRegex = /((http|https):\/\/)(www.)?” + “[a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]” + “{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)/
+    return urlRegex.test(str)
+}
