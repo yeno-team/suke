1b84f5c975fa61d1b40143de2f8e05044e5dccd8
Luna <bedgowns@gmail.com>
2021-10-18 21:28:15 -0700

    isValidUrl util func w/ test

--- packages/suke-util/tests/isValidUrl/index.test.ts
+++ packages/suke-util/tests/isValidUrl/index.test.ts
@@ -1,3 +1,18 @@
 import { isValidUrl } from "@suke/suke-util/src"
 
-console.log(isValidUrl("https://roblox.com"))
+describe("testing isValidUrl util function", () => {
+    test("should return true for valid url's" , () => {
+        expect(isValidUrl("yeno.dev")).toBeTruthy()
+        expect(isValidUrl("sync.yeno.dev")).toBeTruthy()
+        expect(isValidUrl("https://sync.yeno.dev")).toBeTruthy()
+        expect(isValidUrl("http://sync.yeno.dev")).toBeTruthy()
+    })
+
+    test("should return false for invalid url's" , () => {
+        expect(isValidUrl("ftp://iseeyoukhai@yeno.dev")).toBeFalsy()
+        expect(isValidUrl("yeno.d")).toBeFalsy()
+        expect(isValidUrl("http//yeno.d")).toBeFalsy()
+        expect(isValidUrl("dab://yeno.d")).toBeFalsy()
+        expect(isValidUrl("yeno://khai93.com")).toBeFalsy()
+    })
+})
