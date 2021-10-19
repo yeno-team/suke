5aa6cdf2f4aec600118a0d9c2af1053c1ec7b681
Luna <bedgowns@gmail.com>
2021-10-18 20:50:52 -0700

    isValidUrl util function

--- packages/suke-util/src/index.ts
+++ packages/suke-util/src/index.ts
@@ -1,3 +1,4 @@
 export {default as getEnvironmentVariable} from './getEnvironmentVariable';
 export {default as isValidEmail} from './isValidEmail';
 export {default as createFormData} from "./createFormData";
+export {default as isValidUrl} from "./isValidEmail";
