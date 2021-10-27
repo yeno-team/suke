bf6bd78c605a1547e8de24d542dc026833aba991
khai93 <33293519+khai93@users.noreply.github.com>
2021-10-27 04:56:47 -0500

    add className prop

--- packages/suke-web/src/components/StringColor/index.tsx
+++ packages/suke-web/src/components/StringColor/index.tsx
@@ -2,18 +2,18 @@ import { colorShade, stringToColor } from "@suke/suke-util"
 
 
 export interface StringColorProps {
+    className?: string;
     baseString: string;
     brightness: number;
     bold: boolean,
     children: React.ReactNode;
 }
 
-export const StringColor = ({baseString, brightness, bold, children}: StringColorProps) => {
+export const StringColor = ({baseString, brightness, bold, children, className}: StringColorProps) => {
     const color = colorShade(stringToColor(baseString), brightness);
     
- 
     return ( 
-        <div style={{
+        <div className={className} style={{
             color, 
             fontWeight: bold ? 'bold' : 'normal',
             display: 'inline-block',
