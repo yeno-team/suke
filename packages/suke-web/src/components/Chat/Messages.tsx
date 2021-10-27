0cb935b208c35db393dbefb93a0c1ac50f02b4f3
khai93 <33293519+khai93@users.noreply.github.com>
2021-10-27 04:47:59 -0500

    add basic mobile chat

--- packages/suke-web/src/components/Chat/Messages.tsx
+++ packages/suke-web/src/components/Chat/Messages.tsx
@@ -1,19 +1,27 @@
 import { IMessage } from "@suke/suke-core/src/entities/Message";
+import classNames from "classnames";
 import React from "react";
 import { StringColor } from "../StringColor";
 
 export interface MessagesProps {
+    className?: string;
     messages: IMessage[];
 }
 
-export const Messages = ({messages}: MessagesProps) => {
+export const Messages = ({messages, className}: MessagesProps) => {
     return (
-        <div>
+        <div className={classNames(
+            'overflow-auto',
+            'overflow-x-hidden',
+            'whitespace-nowrap',
+            className
+        )}>
             {
                 messages.map(msg => {
                     return (
                         <div>
-                            <StringColor baseString={msg.author.name} brightness={40} bold>{msg.author.name}: </StringColor> {msg.content}
+                            <StringColor className="mr-1" baseString={msg.author.name} brightness={5} bold>{msg.author.name}: </StringColor> 
+                            <div className="pl-1 inline whitespace-normal break-words text-indent-2">{msg.content}</div>
                         </div>
                     )
                 })
