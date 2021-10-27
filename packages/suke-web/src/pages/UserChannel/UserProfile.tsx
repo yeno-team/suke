868766e07e9672816f63fa65414c65dca8aa5345
khai93 <33293519+khai93@users.noreply.github.com>
2021-10-27 04:55:48 -0500

    add UserProfile

--- packages/suke-web/src/pages/UserChannel/UserProfile.tsx
+++ packages/suke-web/src/pages/UserChannel/UserProfile.tsx
@@ -1,14 +1,34 @@
 import React from "react"
-import { Icon as IconElement } from '@iconify/react'
+import { Icon } from '@iconify/react'
+import { ImageCircle } from "../../components/ImageCircle"
+import { Button } from "../../components/Button"
 
 export type UserProfileParams = {
     username: string;
+    followerCount: number;
 } 
 
-export const UserProfile = ({username}: UserProfileParams) => {
+export const UserProfile = ({username, followerCount}: UserProfileParams) => {
     return (
-        <React.Fragment>
-            <IconElement fontSize="18px" icon="ci:share" />
-        </React.Fragment>
+        <div className="flex bg-black font-sans">
+            <div className="flex px-4 py-5 flex-grow">
+                <ImageCircle src="https://picsum.photos/200" alt="user profile pic"></ImageCircle>
+                <div className="flex flex-col justify-center leading-none items-center">
+                    <span className="block font-bold ml-1 text-white">{username}</span>
+                    <span className="font-normal text-sm ml-3 text-gray">{followerCount} followers</span>
+                </div>
+            </div>
+            <div className="mr-4 my-auto">
+                <Button size={2} square backgroundColor="coolorange" fontWeight="semibold" >
+                    <Icon icon="fa-solid:share-alt" className="text-base text-white" />
+                </Button>
+                <Button size={2} square backgroundColor="red" fontWeight="semibold" >
+                    <Icon icon="ic:sharp-report-problem" className="text-base text-white" />
+                </Button>
+                <Button size={3} backgroundColor="teal" fontWeight="semibold" >
+                    <Icon icon="akar-icons:plus" className="text-base mr-1 text-white" /> FOLLOW
+                </Button>
+            </div>
+        </div>
     )
 }
