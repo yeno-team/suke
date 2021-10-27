aabfa4d4eb5eada473d585a3d097ab6f16c680a4
khai93 <33293519+khai93@users.noreply.github.com>
2021-10-27 04:47:59 -0500

    add basic mobile chat

--- packages/suke-web/src/components/Chat/index.tsx
+++ packages/suke-web/src/components/Chat/index.tsx
@@ -1,15 +1,48 @@
 import { IMessage } from '@suke/suke-core/src/entities/Message'
-import React from 'react';
-import './Chat.scss';
+import classNames from 'classnames';
+import React, { useState } from 'react';
 import { Messages } from './Messages';
+import './Chat.css';
 
 export interface ChatProps {
+    className?: string;
     messages: IMessage[];
     submitMessage: (message: IMessage) => void;
 }
 
-export const Chat = ({messages}: ChatProps) => {
+export const Chat = ({messages, submitMessage, className}: ChatProps) => {
+    const [messageInput, setMessageInput] = useState("");
+
+    const handleSubmit = () => {
+        const msg: IMessage = {
+            content: messageInput,
+            author: {
+                id: 1,
+                name: "test"
+            }
+        }
+
+        submitMessage(msg);
+    }
+
     return (
-        <div></div>
+        <div className={classNames(
+            'flex',
+            'bg-coolblack',
+            'flex-col',
+            'font-sans',
+            'h-full',
+            'overflow-auto',
+            className
+        )}>
+            <header className="w-full text-white text-lg tracking-wide text-center p-4 bg-black font-semibold">
+                CHAT
+            </header>
+            <Messages className="text-white p-4 text-sm h-3/4" messages={messages} />
+            <div className="flex m-auto w-full items-center justify-center mb-4">
+                <input value={messageInput} onChange={e => setMessageInput(e.target.value)} className="w-3/4 p-3 rounded-tl rounded-bl text-sm" placeholder="Send message..." type="text"></input>
+                <button onClick={handleSubmit} className="bg-teal rounded-tr text-sm  rounded-br px-2 py-3 mt-0 text-white w-14">Send</button>
+            </div>
+        </div>
     )
 }
