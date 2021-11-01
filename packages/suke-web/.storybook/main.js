44da57664b023557acb369af9fd02a90bd61eb19
Luna <bedgowns@gmail.com>
2021-11-01 01:14:12 -0700

    story components now render tailwind styles.

--- packages/suke-web/.storybook/main.js
+++ packages/suke-web/.storybook/main.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
   "typescript": {
     "check": false,
@@ -16,5 +18,24 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app"
-  ]
+  ],
+  "webpackFinal": async (config) => {
+    config.module.rules.push({
+      test: /\,css&/,
+      use: [
+        {
+          loader: 'postcss-loader',
+          options: {
+            ident: 'postcss',
+            plugins: [
+              require('tailwindcss'),
+              require("autoprefixer")
+            ]
+          }
+        }
+      ],
+      include: path.resolve(__dirname, '../'),
+    })
+    return config
+  }
 }
