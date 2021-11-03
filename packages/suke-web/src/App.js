dfaa9e82183318741963c6b83f26b35cea9d43cb
khai93 <33293519+khai93@users.noreply.github.com>
2021-11-03 06:51:28 -0500

    add useAuth hook

--- packages/suke-web/src/App.js
+++ packages/suke-web/src/App.js
@@ -1,14 +1,16 @@
 import { BrowserRouter } from 'react-router-dom';
-
 import { ThemeContext } from './context/Theme';
 import { Routes } from './routes';
+import { AuthProvider } from './hooks/useAuth';
 
 
 function App() {
   return (
     <ThemeContext.Provider>
       <BrowserRouter>
-        <Routes />
+        <AuthProvider>
+          <Routes />
+        </AuthProvider>
       </BrowserRouter>
     </ThemeContext.Provider>
   );
