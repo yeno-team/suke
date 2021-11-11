ecb9e75e1d31f3999fdb017c50b663997a772451
khai93 <33293519+khai93@users.noreply.github.com>
2021-11-11 10:28:34 -0600

    join room in channel page

--- packages/suke-web/src/pages/UserChannel/index.tsx
+++ packages/suke-web/src/pages/UserChannel/index.tsx
@@ -1,8 +1,8 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { useParams } from "react-router-dom";
 import { Navigation } from "../../common/Navigation"
-import { Chat } from "../../components/Chat";
 import { VideoMenu } from "../../components/VideoMenu";
+import { useRoom } from "../../hooks/useRoom";
 import { ChatBox } from "./ChatBox";
 import { UserProfile } from "./UserProfile";
 
@@ -13,11 +13,19 @@ type UserChannelPageParams = {
 
 export const UserChannelPage = (): JSX.Element => {
     const { username } = useParams<UserChannelPageParams>();
+
+    const { joinRoom } = useRoom();
+
+    useEffect(() => {
+        joinRoom(username);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [username])
+
     return (
         <div className="h-screen flex flex-col">
             <Navigation />
             <VideoMenu />
-            <ChatBox />
+            <ChatBox username={username} />
             <UserProfile username={username} followerCount={3}/>
         </div>
     )
