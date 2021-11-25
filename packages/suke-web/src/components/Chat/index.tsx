88cff2e11f63283ddc24661519e7cdf78ed0fe29
Luna <bedgowns@gmail.com>
2021-11-25 02:29:00 -0800

    replaced input as textarea

--- packages/suke-web/src/components/Chat/index.tsx
+++ packages/suke-web/src/components/Chat/index.tsx
@@ -1,7 +1,8 @@
 import { IMessage } from '@suke/suke-core/src/entities/Message';
-import { Icon, InlineIcon } from '@iconify/react';
+import { Icon } from '@iconify/react';
 import classNames from 'classnames';
-import React, { useState , useRef, useLayoutEffect } from 'react';
+import React, {useState } from 'react';
+import TextAreaAutoResize from "react-textarea-autosize";
 import { Messages } from './Messages';
 import './Chat.css';
 
@@ -13,7 +14,8 @@ export interface ChatProps {
 }
 
 export const Chat = ({messages, submitMessage, className , channelName }: ChatProps) => {
-    const [messageInput, setMessageInput] = useState("");
+    const [ messageInput, setMessageInput ] = useState("");
+
     const serializeInputMessage = () : IMessage => {
         return {
             content : messageInput,
@@ -29,19 +31,18 @@ export const Chat = ({messages, submitMessage, className , channelName }: ChatPr
         setMessageInput("")
     }
 
-    const handleSubmitOnKeyPress = (key : string) => {
-        console.log(key)
-        if(key !== "Enter") {
-            return null
+    const handleSubmitByEnter = (e : React.KeyboardEvent) => {
+        if(e.key === "Enter" && !(e.shiftKey)) {
+            e.preventDefault()
+            handleSubmit()
         }
-
-        handleSubmit()
     }
 
     // Handles the logic when the client wants to reply to a user in the chat.
     const replyHandler = (authorName : string) : void => {
         setMessageInput(`${messageInput} @${authorName}`)
     }
+
 
     return (
         <div className={classNames(
@@ -53,8 +54,8 @@ export const Chat = ({messages, submitMessage, className , channelName }: ChatPr
             <Messages className="text-white p-4 flex-1 text-sm xl:text-base overflow-y-scroll" messages={messages} channelName="among us" replyHandler={replyHandler}/>
             <div className="p-5">
                 <div className="w-full rounded-md flex items-center bg-coolgray rounded-md pr-5">
-                    <textarea value={messageInput} onChange={e => setMessageInput(e.target.value)} onKeyUp={(e) => handleSubmitOnKeyPress(e.key)} className="p-3 rounded-l-md text-sm md:text-base focus:outline-none text-white resize-none overflow-hidden bg-transparent flex-1" maxLength={500} rows={1} placeholder="Send message..."/>
-                    <Icon icon="fa-solid:sad-cry" className="text-white cursor-pointer" height={24} width={24}/>
+                    <TextAreaAutoResize value={messageInput} maxRows={3} onChange={e => setMessageInput(e.target.value)} className="p-3 rounded-l-md text-sm md:text-base focus:outline-none text-white resize-none overflow-hidden bg-transparent flex-1 h-auto" maxLength={500} placeholder="Send message..." onKeyDown={handleSubmitByEnter}/>
+                    <Icon icon="fa-solid:sad-cry" className="text-white cursor-pointer" height={24} width={24} onClick={handleSubmit}/>
                 </div>
             </div>
         </div>
