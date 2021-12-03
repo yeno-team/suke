f52d96f305c9fdb0a5ad82ec1d1215053205be78
Luna <bedgowns@gmail.com>
2021-12-02 19:14:05 -0800

    KAA parser query

--- packages/wrappers/src/kickassanime/test.ts
+++
@@ -1,6 +0,0 @@
-import Container from "typedi";
-import { KickAssAnimeApiWrapper } from "./wrapper";
-
-const among = Container.get(KickAssAnimeApiWrapper)
-
-among.getVideoPlayerUrl(new URL("https://www2.kickassanime.ro/anime/rakudai-kishi-no-cavalry-383660/episode-12-110077")).then(console.log)
