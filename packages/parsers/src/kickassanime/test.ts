8758b63cbeeef495eb176c6e183f6eea69ef1fdc
Luna <bedgowns@gmail.com>
2021-12-05 19:26:04 -0800

    removed test.ts

--- packages/parsers/src/kickassanime/test.ts
+++
@@ -1,6 +0,0 @@
-import { ParserSearchOptions } from "@suke/suke-core/src/entities/Parser";
-import Container from "typedi";
-import KickAssAnimeParser from ".";
-
-const a = Container.get(KickAssAnimeParser)
-a.search("Boruto", new ParserSearchOptions({ pageNumber : 1 })).then(console.log).catch(console.error)
