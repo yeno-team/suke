8f143346a6d50459ca3623fa767aa4ddb741935c
khai93 <33293519+khai93@users.noreply.github.com>
2021-12-10 06:17:17 -0600

    add channelHandler

--- packages/suke-socket-server/src/handlers/channel/index.ts
+++ packages/suke-socket-server/src/handlers/channel/index.ts
@@ -6,26 +6,78 @@ import { SocketServer, WebSocketConnection } from "../../server";
 import { Handler } from "../Handler";
 import { getRepository } from "typeorm";
 import { UserChannelModel } from "@suke/suke-core/src/entities/UserChannel/UserChannel";
+import { SocketBroadcaster } from "../../extensions/Broadcaster";
+import { RequestManager } from "../../extensions/RequestManager";
 
 
 export const createChannelHandler: Handler = (server: SocketServer) => (): void => {
-    server.on('message', (message: SocketMessage, ws: WebSocketConnection, user?: User) => {
+    server.on('message', async (message: SocketMessage, ws: WebSocketConnection, user?: User) => {
         const msg = message as SocketMessageInput; // For type-safe data type
         const channelManager = new ChannelManager(server);
 
-        // some how get the channel repository
-        const channelRepo = getRepository(UserChannelModel)
+        const broadcaster = new SocketBroadcaster(server);
+        const channelRepo = getRepository(UserChannelModel);
+        const requestManager = new RequestManager(server);
         const users = server.getUserConnections();
 
         if (user.Id().Equals(new UserId(0))) {
-            return server.emit('clientError', new Error("You do not have permission to edit the room settings."), ws)
+            return server.emit('clientError', new Error("You do not have permission to use this event."), ws)
         }
 
-        
-
         switch(msg.type) {
-            case 'ROOM_UPDATE':
+            case 'CHANNEL_REQUEST_ADD':
+                try {
+                    await requestManager.addRequest(msg.data.roomId, msg.data);
+
+                    broadcaster.broadcastToRoom(new SocketMessage({
+                        type: "CHANNEL_REQUEST_ADD",
+                        data: msg.data
+                    }), msg.data.roomId);
+                } catch (e) {
+                    server.emit('error', e);
+                }
+                break;
+            case 'CHANNEL_REQUEST_REMOVE':
+                try {
+                    await requestManager.removeRequest(msg.data.roomId, msg.data);
+
+                    broadcaster.broadcastToRoom(new SocketMessage({
+                        type: "CHANNEL_REQUEST_REMOVE",
+                        data: msg.data
+                    }), msg.data.roomId);
+                } catch (e) {
+                    server.emit('error', e);
+                }
+                break;
+            case 'CHANNEL_REQUESTS_GET':
+                try {
+                    const requests = await requestManager.getRequests(msg.data)
+                    ws.send(JSON.stringify(new SocketMessage({
+                        type: 'CHANNEL_REQUESTS',
+                        data: requests
+                    })));
+                } catch (e) {
+                    server.emit('error', e);
+                }
+                break;
+            case 'CHANNEL_UPDATE':
+                try {
+                    const updated = await channelManager.editRealtimeChannel(msg.data.channelId, msg.data);
                 
+                    if (updated) {
+                        broadcaster.broadcastToRoom(new SocketMessage({
+                            type: 'CHANNEL_UPDATE',
+                            data: msg.data
+                        }), msg.data.channelId)
+                    } else {
+                        ws.send(new SocketMessage({
+                            type: 'SERVER_ERROR',
+                            data: "Could not update channel data."
+                        }));
+                    }
+                } catch (e) {
+                    server.emit('clientError', e, ws)
+                }
                 break;
         }
     }) 
