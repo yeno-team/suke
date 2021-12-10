dd93d953fa9f66f76a7cf64a033c5442759d5014
khai93 <33293519+khai93@users.noreply.github.com>
2021-12-10 06:23:10 -0600

    add useChannel hook

---
+++ packages/suke-web/src/hooks/useChannel.ts
@@ -0,0 +1,85 @@
+import { isRequestsEqual, Request } from "@suke/suke-core/src/entities/Request";
+import { SocketMessage, SocketMessageInput } from "@suke/suke-core/src/entities/SocketMessage";
+import { RealtimeChannelData } from "@suke/suke-core/src/types/UserChannelRealtime";
+import { useEffect, useState } from "react";
+import { useChanged } from "./useChanged";
+import { useSocket } from "./useSocket";
+
+export const useChannel = () => {
+    const [requests, setRequests] = useState<Request[]>([]);
+    const { send } = useSocket();
+    const { messages } = useSocket();
+    const [ socketMessagesChanged, prevSocketMessages] = useChanged<SocketMessage[]>(messages);
+    
+    useEffect(() => {
+        if (!socketMessagesChanged || prevSocketMessages == null)
+            return;
+
+        const newMessages = messages.slice(prevSocketMessages.length);
+        const newRequests: Request[] = [];
+        const removedRequests: Request[] = [];
+
+        const addNewRequest = (req: Request) => {
+            const foundRequest = newRequests.find(v => isRequestsEqual(v, req));
+            if (foundRequest == null) {
+                newRequests.push(req);
+            }
+        }
+
+        for (const msg of newMessages) {
+            const typedMsg = msg as SocketMessageInput;
+            switch (typedMsg.type) {
+                case "CHANNEL_REQUEST_ADD":
+                    addNewRequest(typedMsg.data)
+                    break;
+                case "CHANNEL_REQUEST_REMOVE": // TODO: IMPLEMENT THIS
+                    removedRequests.push(typedMsg.data)
+                    break;
+                case "CHANNEL_REQUESTS":
+                    for (const req of typedMsg.data) {
+                        addNewRequest(req);
+                    }
+                    break;
+            }
+        }
+
+        setRequests([
+            ...requests,
+            ...newRequests
+        ].filter(v => removedRequests.find(removed => isRequestsEqual(v, removed)) == null));
+    }, [messages, prevSocketMessages, requests, socketMessagesChanged])
+
+
+    const createRequest = (request: Request) => {
+        send({
+            type: 'CHANNEL_REQUEST_ADD',
+            data: request
+        });
+    }
+
+    const removeRequest = (request: Request) => {
+        send({
+            type: 'CHANNEL_REQUEST_REMOVE',
+            data: request
+        });
+    }
+
+    const getRequests = (roomId: string) => {
+        send({
+            type: 'CHANNEL_REQUESTS_GET',
+            data: roomId
+        });
+    }
+
+    const updateRealtimeChannelData = (updatedChannelData: RealtimeChannelData & { channelId: string }) => {
+        send({
+            type: 'CHANNEL_UPDATE',
+            data: {
+                channelId: updatedChannelData.channelId,
+                currentVideo: updatedChannelData.currentVideo
+            }
+        });
+    }
+
+    return { createRequest, removeRequest, updateRealtimeChannelData, requests, getRequests};
+}
