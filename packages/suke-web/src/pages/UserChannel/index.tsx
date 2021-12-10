cd2999a6f7d2fa0dd9acdcc5406fb0bf87016ef6
khai93 <33293519+khai93@users.noreply.github.com>
2021-12-10 06:25:18 -0600

    fix KickAssAnime Wrapper

--- packages/suke-web/src/pages/UserChannel/index.tsx
+++ packages/suke-web/src/pages/UserChannel/index.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react"
 import { useParams } from "react-router-dom";
 import { Navigation } from "../../common/Navigation"
 import { VideoMenu } from "../../components/VideoMenu";
+import useAuth from "../../hooks/useAuth";
 import { useRoom } from "../../hooks/useRoom";
 import { BrowserModal } from "./BrowserModal";
 import { ChatBox } from "./ChatBox";
@@ -17,7 +18,8 @@ export const UserChannelPage = (): JSX.Element => {
     const [browserActive, setBrowserActive] = useState(false);
     const { username } = useParams<UserChannelPageParams>();
     const { joinRoom } = useRoom();
-
+    const { user } = useAuth();
+    
     useEffect(() => {
         joinRoom(username);
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -27,15 +29,15 @@ export const UserChannelPage = (): JSX.Element => {
         setBrowserActive(!browserActive);
     }
 
-    const mobileClassListIfBrowserActive = browserActive ? "hidden md:block" : "block";
+    const mobileClassListIfBrowserActive = browserActive ? "hidden lg:block" : "block";
 
     return (
         <div className="h-screen flex flex-col">
             <Navigation className={mobileClassListIfBrowserActive} />
-            <BrowserModal className="flex-grow" active={browserActive} setActive={setBrowserActive} />
-            <VideoMenu className={mobileClassListIfBrowserActive} url="https://www.youtube.com/watch?v=NpEaa2P7qZI" handleOpenBrowser={toggleBrowserActive} />
+            <BrowserModal roomId={username} className="flex-grow" active={browserActive} setActive={setBrowserActive} />
+            <VideoMenu className={mobileClassListIfBrowserActive} url="https://www.youtube.com/watch?v=NpEaa2P7qZI" handleOpenBrowser={toggleBrowserActive} isAuthenticated={user?.id !== 0}/>
             <ChatBox className={mobileClassListIfBrowserActive} username={username} />
-            <UserProfile className={mobileClassListIfBrowserActive} username={username} followerCount={3}/>
+            <UserProfile className={mobileClassListIfBrowserActive} username={username} followerCount={3} />
         </div>
     )
 }
