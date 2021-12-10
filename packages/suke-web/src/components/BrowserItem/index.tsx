f5a122c578a80203437e29a839c6f41a28bce602
khai93 <33293519+khai93@users.noreply.github.com>
2021-12-10 06:21:35 -0600

    add Requests to BrowerItem

--- packages/suke-web/src/components/BrowserItem/index.tsx
+++ packages/suke-web/src/components/BrowserItem/index.tsx
@@ -1,22 +1,82 @@
+import { Icon } from "@iconify/react";
+import { IMultiData, IMultiStandaloneData, IStandaloneData } from "@suke/suke-core/src/entities/SearchResult";
+import { UserId } from "@suke/suke-core/src/entities/UserId";
+import classNames from "classnames";
+import { useEffect, useState } from "react";
+import useAuth from "../../hooks/useAuth";
+import { useChannel } from "../../hooks/useChannel";
 import { Button } from "../Button";
+import { Request } from "@suke/suke-core/src/entities/Request";
 
 export interface BrowserItemProps {
-    thumbnailUrl: string,
-    title: string,
-    category: string
+    key: string,
+    data: IStandaloneData,
+    category: string,
+    roomId: string,
+    requestedBy?: string[]
 }
 
-export function BrowserItem({ thumbnailUrl, title, category }: BrowserItemProps) {
+export interface MultiBrowserItemProps {
+    key: string,
+    data: IMultiData,
+    category: string,
+    roomId: string,
+    requestedStandalones?: IMultiStandaloneData[],
+    requestedBy?: string[]
+}
+
+export interface MultiBrowserStandaloneItemProps {
+    key: string,
+    data: IMultiData,
+    roomId: string,
+    standaloneData: IMultiStandaloneData;
+    requestedBy?: string[]
+}
+
+export function BrowserItem({data, category, roomId, requestedBy }: BrowserItemProps) {
+    const { createRequest, removeRequest } = useChannel();
+    const { user } = useAuth();
+    
+    const requestObj: Request = {
+        requestType: 'standalone',
+        requestedData: data,
+        requestedBy: [{
+            userId: new UserId(user?.id as number),
+                name: user?.name as string,
+            }],
+            roomId
+    };
+
+    const handleRequest = () => {
+        createRequest(requestObj);
+    }
+
+    const handleUndoRequest = () => {
+        removeRequest(requestObj)
+    }
+
     return (
-        <div className="container font-sans">
-            <div className="w-28 object-cover inline-block">
-                <img src={thumbnailUrl} className="h-24" alt={`${title} thumbnail`}></img>
-            </div> 
-            <div className="inline-block text-white">
-                <h1 className="font-bold ">{title}</h1>
-                <h3>{category}</h3>
+        <div className="font-sans bg-coolblack mb-2 flex m-0">
+            <img src={data.thumbnail_url!} className="m-0 max-w-128 w-auto h-auto object-cover" alt={`${data.name} thumbnail`}></img>
+            <div className="inline-block text-white align-middle m-0 ml-2 py-2 flex-grow">
+                <h1 className="font-bold text-sm">{data.name?.toUpperCase()}</h1>
+                <h3 className="m-0 p-0 text-xs">{category}</h3>
+                {
+                    requestedBy ? <h4 className="text-xs font-signika text-blue font-bold">REQUESTED BY: {requestedBy.join(", ")}</h4> : null
+                }
+                {
+                    requestedBy && requestedBy.findIndex(v => v.toLowerCase() === user?.name.toLowerCase()) !== -1 ?
+                    <Button className={classNames("m-0 mt-3 rounded-md relative")} fontWeight="semibold" size={3} fontSize="xs" onClick={handleUndoRequest} backgroundColor={ "red"}>
+                        Revert
+                    </Button> :
+                    <Button className={classNames("m-0 mt-3 rounded-md relative")} fontWeight="semibold" size={3} fontSize="xs" onClick={handleRequest} backgroundColor={ "blue"}>
+                        Request  
+                    </Button> 
+                }
+                
             </div>
-            <Button>REQUEST</Button>
         </div>
     )
 }
+
+export * from "./MultiBrowserItem";
