98b02e7958fdcb7c89b99097356e656b34e077ac
khai93 <33293519+khai93@users.noreply.github.com>
2021-12-10 06:18:28 -0600

    await updated methods

--- packages/suke-socket-server/src/handlers/chat/index.ts
+++ packages/suke-socket-server/src/handlers/chat/index.ts
@@ -5,18 +5,15 @@ import { Handler } from "../Handler"
 
 
 export const createChatMessageHandler: Handler = (server: SocketServer) => (): void => {
-    server.on('message', (msg, ws: WebSocketConnection) => {
-
+    server.on('message', async (msg, ws: WebSocketConnection) => {
         const message = msg as SocketMessageInput;
 
         const broadcaster = new SocketBroadcaster(server);   
         const roomManager = server.getRoomManager();
 
         if (message.type == 'CHAT_MESSAGE') {
-            if (!roomManager.CheckIfUserInRoom(ws, message.data.channelId))
-                return server.emit('clientError', new Error("Cannot send message."), ws);
-        
-            broadcaster.broadcastToRoom(msg, message.data.channelId);
+            if (!(await roomManager.CheckIfUserInRoom(ws.id, message.data.channelId))) return server.emit('clientError', new Error("Cannot send message."), ws);
+            await broadcaster.broadcastToRoom(msg, message.data.channelId);
         }
     });
 }
