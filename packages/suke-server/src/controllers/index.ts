2e4023fdbded4476263697291e972d3b78e23f46
khai93 <33293519+khai93@users.noreply.github.com>
2021-12-16 06:42:36 -0600

    add source get endpoint

--- packages/suke-server/src/controllers/index.ts
+++ packages/suke-server/src/controllers/index.ts
@@ -3,11 +3,13 @@ import { UserChannelController } from "./channel";
 import { SourceListController } from "./source/list";
 import { SourceSearchController } from "./source/search";
 import { UserController } from "./user";
+import { SourceGetController } from "./source/get";
 
 export default [
     AuthController,
     UserChannelController,
     UserController,
     SourceSearchController,
-    SourceListController
+    SourceListController,
+    SourceGetController
 ]
