41461b779e19944c4641cb9a3f770142d12cbcb6
khai93 <33293519+khai93@users.noreply.github.com>
2021-12-16 06:39:03 -0600

    add realtime channel data

--- packages/suke-core/src/types/UserChannelRealtime.ts
+++ packages/suke-core/src/types/UserChannelRealtime.ts
@@ -1,12 +1,13 @@
-import { Author } from "../entities/User";
 import { IVideoSource } from "../entities/SearchResult";
 
-export type VideoRequest = {
-    video: IVideoSource
-    author: Author;
-}
-
 export interface RealtimeChannelData {
-    currentVideo: IVideoSource;
-    requests: VideoRequest[];
+    currentVideo: {
+        sources: IVideoSource[],
+        name: string, 
+        category: string
+    };
+    paused: boolean;
+    progress: {
+        currentTime: number
+    }
 }
