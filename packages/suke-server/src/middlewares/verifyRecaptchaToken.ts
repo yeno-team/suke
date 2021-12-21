e8065da86c958c108551a60ef00edf14ada983db
Luna <bedgowns@gmail.com>
2021-12-20 22:13:29 -0800

    captcha on login page

--- packages/suke-server/src/middlewares/verifyRecaptchaToken.ts
+++ packages/suke-server/src/middlewares/verifyRecaptchaToken.ts
@@ -7,16 +7,25 @@ import config from "../config"
 export const verifyRecaptchaToken = () : RequestHandler => catchErrorAsync(async(req , res , next) => {
     const recaptchaApiWrapper = Container.get(RecaptchaApiWrapper)
 
-    console.log(await recaptchaApiWrapper.verifyToken({
+    const resp = await recaptchaApiWrapper.verifyToken({
         secretKey : config.recaptcha.secretKey,
         token : req.body.reCaptchaToken,
         ipAddress : req.ip
-    }))
+    })
 
+    if(!(resp.success)) {
+        return res.status(400).json({
+            success : false,
+            message : JSON.stringify(resp["error-codes"])
+        })
+    }
+
+    if(resp.score < 0.8) {
+        return res.status(400).json({
+            success : false,
+            message : "Client interactions poses high risk and might be fradulent. Please try again later."
+        })
+    }
 
     next()
-    // recaptchaApiWrapper.verifyToken({
-    //     secret
-    // })
-
 })
