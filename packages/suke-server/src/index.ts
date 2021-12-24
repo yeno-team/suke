77ea8e4c96e7f962c8137efee4164d52f028a477
khai93 <33293519+khai93@users.noreply.github.com>
2021-12-23 21:21:14 -0600

    remove unused service

--- packages/suke-server/src/index.ts
+++ packages/suke-server/src/index.ts
@@ -1,5 +1,5 @@
 import 'reflect-metadata';
-import config,{ RedisClient } from './config';
+import config, { RedisClient } from './config';
 import { Container } from 'typedi';
 import { Container as typeORMContainer } from 'typeorm-typedi-extensions';
 import { createConnection, useContainer } from 'typeorm';
@@ -8,6 +8,7 @@ import { UserModel } from '@suke/suke-core/src/entities/User';
 import { UserChannelModel } from '@suke/suke-core/src/entities/UserChannel';
 import redis from 'redis';
 import { SessionModel } from '@suke/suke-core/src/entities/Session';
+import { Follower } from '@suke/suke-core/src/entities/Follower';
 
 useContainer(typeORMContainer);
 
@@ -15,7 +16,7 @@ createConnection({
     type: "postgres",
     url: config.db.connectionUri,
     logger: 'advanced-console',
-    entities: [UserModel, UserChannelModel, SessionModel],
+    entities: [UserModel, UserChannelModel, SessionModel, Follower],
     synchronize: true,
 }).then(() => {
     Container.set<redis.RedisClientType>('redis', RedisClient);
@@ -27,4 +28,3 @@ createConnection({
     console.error(`Couldn't connect to the database!`);
     console.error(error);
 });
-
