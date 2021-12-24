566de9efc91742cc775402f30210173e63b82b4c
khai93 <33293519+khai93@users.noreply.github.com>
2021-12-23 21:22:12 -0600

    add User Unfollow Controller

--- packages/suke-server/src/controllers/index.ts
+++ packages/suke-server/src/controllers/index.ts
@@ -4,10 +4,14 @@ import { SourceListController } from "./source/list";
 import { SourceSearchController } from "./source/search";
 import { UserController } from "./user";
 import { SourceGetController } from "./source/get";
+import { UserChannelFollowController } from "./follow";
+import { UserChannelUnfollowController } from "./unfollow";
 
 export default [
     AuthController,
     UserChannelController,
+    UserChannelFollowController,
+    UserChannelUnfollowController,
     UserController,
     SourceSearchController,
     SourceListController,
