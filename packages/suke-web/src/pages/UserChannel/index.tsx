7f3b668bb392b382236d529627109dab0f0425d2
khai93 <33293519+khai93@users.noreply.github.com>
2021-12-23 21:30:39 -0600

    add follow feature

--- packages/suke-web/src/pages/UserChannel/index.tsx
+++ packages/suke-web/src/pages/UserChannel/index.tsx
@@ -1,6 +1,8 @@
 
+import { IUserChannel } from "@suke/suke-core/src/entities/UserChannel/UserChannel";
 import React, { useEffect, useState } from "react"
 import { useParams } from "react-router-dom";
+import { followChannel, getChannel, unfollowChannel } from "../../api/channel";
 import { Navigation } from "../../common/Navigation"
 import { VideoMenu } from "../../components/VideoMenu";
 import useAuth from "../../hooks/useAuth";
@@ -17,13 +19,27 @@ type UserChannelPageParams = {
 
 export const UserChannelPage = (): JSX.Element => {
     const [browserActive, setBrowserActive] = useState(false);
+    const [channel, setChannel] = useState<(Omit<IUserChannel, 'followers'> & {followers: number}) | null>();
+    const [searching, setSearching] = useState(true);
     const { username } = useParams<UserChannelPageParams>();
     const { joinRoom } = useRoom();
-    const { user } = useAuth();
+    const { user, updateUser } = useAuth();
     const { channelData } = useChannel();
     
     useEffect(() => {
-        joinRoom(username);
+        const sendGetChannel = async () => {
+            try {
+                setSearching(true);
+                const channelResp = await getChannel(username);
+                setChannel(channelResp);
+            } catch (e) {
+                console.warn(e);
+            } finally { 
+                setSearching(false);
+                joinRoom(username);
+            }
+        }
+        sendGetChannel();
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [username])
 
@@ -31,15 +47,39 @@ export const UserChannelPage = (): JSX.Element => {
         setBrowserActive(!browserActive);
     }
 
-    const mobileClassListIfBrowserActive = browserActive ? "hidden lg:block" : "block";
+    const handleFollow = async () => {
+        try {
+            await followChannel(username);
+            updateUser();
+        } catch (e) {
+            console.warn(e);
+        }
+    }
 
+    const handleUnfollow = async () => {
+        try {   
+            await unfollowChannel(username);
+            updateUser();
+        } catch (e) {
+            console.warn(e);
+        }
+    }
+
+    const mobileClassListIfBrowserActive = browserActive ? "hidden lg:block" : "block";
+    const alreadyFollowed = user?.following.find(v => v.followedTo?.id === channel?.id) != null;
+    
     return (
+        !searching && channel != null ?
         <div className="h-screen flex flex-col">
             <Navigation className={mobileClassListIfBrowserActive} />
             <BrowserModal roomId={username} className="flex-grow" active={browserActive} setActive={setBrowserActive} />
             <VideoMenu className={mobileClassListIfBrowserActive} url={channelData?.currentVideo?.sources[0].url} handleOpenBrowser={toggleBrowserActive} isAuthenticated={user?.id !== 0} title={channelData?.currentVideo?.name} category={channelData?.currentVideo?.category}/>
             <ChatBox className={mobileClassListIfBrowserActive} username={username} />
-            <UserProfile className={mobileClassListIfBrowserActive} username={username} followerCount={3} />
+            <UserProfile className={mobileClassListIfBrowserActive} username={username} followerCount={channel?.followers ?? 0} followed={alreadyFollowed} handleFollow={handleFollow} handleUnfollow={handleUnfollow}/>
+        </div> : 
+        <div className="h-screen flex flex-col">
+            <Navigation className={mobileClassListIfBrowserActive} />
+            <div>Channel doesn't exist :(</div>
         </div>
     )
 }
