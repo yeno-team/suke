e4df335e465d4dd7c4dc8cc72a91a7c6ea7358c9
khai93 <33293519+khai93@users.noreply.github.com>
2022-01-06 23:06:01 -0600

    add desktop browser page

--- packages/suke-web/src/components/VideoMenu/VideoMenuHeader.tsx
+++ packages/suke-web/src/components/VideoMenu/VideoMenuHeader.tsx
@@ -16,26 +16,25 @@ export const VideoMenuHeader = ({viewerCount, handleOpenBrowser, isAuthenticated
             'w-full',
             'flex',
             'bg-coolblack',
-            'px-5 py-3'
+            'px-5 py-3 lg:py-4'
         )}>
             <span className="text-red font-semibold flex-grow flex leading-none">
-                <div className="font-signika inline-block">
-                    <p className="text-lg font-bold text-white mb-0 pb-0 leading-none inline-block">{ title ?? "Untitled Video" }</p>
+                <div className="font-signika inline-block lg:ml-6">
+                    <p className="text-lg font-bold text-white mb-0 pb-0 leading-none inline-block lg:text-2xl lg:leading-none">{ title ?? "Untitled Video" }</p>
                     <p className="text-sm font-light text-gray leading-none">{ category ?? "Unknown Category" }</p>
-                    <div className="inline-block w-12">
+                </div>
+                <div className="inline-block w-12 ml-2">
                     <IconElement className="inline-block" icon="bi:person-fill" color="#C74545"/>
-            
-                    <span className="text-sm leading-none">
+                    <span className="text-sm leading-none font-sans ml-px">
                         {
                             viewerCount ? viewerCount : 0
                         }
                     </span>
                 </div>
-                </div>
                 
             </span>
             {
-               isAuthenticated ? <button onClick={handleOpenBrowser} className="p-2 text-sm font-bold rounded justify-self-end text-white bg-blue ml-4">Open Browser</button> : null 
+               isAuthenticated ? <button onClick={handleOpenBrowser} className="p-2 text-sm font-bold rounded justify-self-end text-white bg-blue lg:p-3">Open Browser</button> : null 
             }
         </header>          
     )
