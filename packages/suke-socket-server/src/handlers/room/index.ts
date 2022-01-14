a6eca5035b8b2609350bb7ecc9fe8877d45bbd33
khai93 <33293519+khai93@users.noreply.github.com>
2022-01-14 03:03:29 -0600

    send update when user joins or leaves

--- packages/suke-socket-server/src/handlers/room/index.ts
+++ packages/suke-socket-server/src/handlers/room/index.ts
@@ -1,27 +1,71 @@
 import { SocketMessage, SocketMessageInput } from "@suke/suke-core/src/entities/SocketMessage";
+import { SocketBroadcaster } from "../../extensions/Broadcaster";
+import { ChannelManager } from "../../extensions/ChannelManager";
 import { SocketServer, WebSocketConnection } from "../../server";
 import { Handler } from "../Handler";
+import { getRepository } from "typeorm";
+import { CategoryModel } from "@suke/suke-core/src/entities/Category";
 
 export const createRoomJoinHandler: Handler = (server: SocketServer) => (): void => {
+
     server.on('message', async (message: SocketMessage, ws: WebSocketConnection) => {
         const msg = message as SocketMessageInput; // For type-safe data type
         const roomManager = server.getRoomManager();
-        
+        const channelManager = new ChannelManager(server);
+        const broadcaster = new SocketBroadcaster(server);
+        const categoryManager = server.getCategoryManager();
+
+        const setupListeners = {};
+
+        const sendUpdateMessage = async (roomId: string) => {
+            const updatedRoom = await roomManager.getRoom(roomId);
+            const roomConnectionsNoDuplicates = {};
+
+            for (const id of updatedRoom) {
+                roomConnectionsNoDuplicates[server.getConnection(id).remoteAddress] = id;
+            }
+            
+            const updated = await channelManager.editRealtimeChannel(roomId, {viewerCount: Math.max(Object.keys(updatedRoom).length - 1, 0)});
+            
+            if (updated != null) {
+                categoryManager.updateRoomViewerCount(roomId, updated.category, updated.viewerCount);
+                broadcaster.broadcastToRoom(new SocketMessage({
+                    type: 'CHANNEL_UPDATE',
+                    data: updated
+                }), roomId);
+            } else {
+                ws.send(JSON.stringify(new SocketMessage({
+                    type: 'SERVER_ERROR',
+                    data: "Could not update channel data."
+                })));
+            }
+
+            if (setupListeners[ws.id] == null) {
+                setupListeners[ws.id] = 1;
+
+                ws.on('close', () => {
+                    server.emit('message', new SocketMessage({type: 'ROOM_LEAVE', data: {roomId}}), ws)
+                    setupListeners[ws.id] = null;
+                });
+            }
+        }
+
         switch (msg.type) {
             case 'ROOM_JOIN':
                 if (await roomManager.CheckIfUserInRoom(ws.id, msg.data.roomId)) {
                     return server.emit('clientError', new Error("Already joined room"), ws);
                 }
-
+                
                 await roomManager.addUser(msg.data.roomId, ws.id);
-
+                await sendUpdateMessage(msg.data.roomId);
                 break;
             case 'ROOM_LEAVE':
-                if (!roomManager.CheckIfUserInRoom(ws.id, msg.data.roomId)) {
+                if (!await roomManager.CheckIfUserInRoom(ws.id, msg.data.roomId)) {
                     return server.emit('clientError', new Error("Cannot leave room that you have not joined."), ws);
                 }
 
                 await roomManager.removeUser(msg.data.roomId, ws.id);
+                await sendUpdateMessage(msg.data.roomId);
                 break;
         }
     })
