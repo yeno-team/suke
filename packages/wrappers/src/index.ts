127632ea9ac8efe4e889324bd95ce6900eee1951
khai93 <33293519+khai93@users.noreply.github.com>
2022-02-02 09:27:32 -0600

    fix lint

--- packages/wrappers/src/index.ts
+++ packages/wrappers/src/index.ts
@@ -2,7 +2,6 @@ import { YoutubeApiWrapper } from "./youtube";
 import { KickAssAnimeApiWrapper } from "./kickassanime";
 import { FzMoviesWrapper } from "./fzmovies";
 import { MobileTvShowsWrapper } from "./mobiletvshows";
-import Container from "typedi";
 
 export {
     YoutubeApiWrapper,
