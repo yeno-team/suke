f832ac05b67f8057ba3fced870a812df278d8e2a
Luna <bedgowns@gmail.com>
2022-02-08 23:41:21 -0800

    parse gogoplay sources hotfix

--- packages/wrappers/src/index.ts
+++ packages/wrappers/src/index.ts
@@ -1,11 +1,13 @@
+import { GogoPlayApiWrapper } from "./gogoplay";
 import { YoutubeApiWrapper } from "./youtube";
 import { KickAssAnimeApiWrapper } from "./kickassanime";
 import { FzMoviesWrapper } from "./fzmovies";
 import { MobileTvShowsWrapper } from "./mobiletvshows";
 
 export {
+    GogoPlayApiWrapper,
     YoutubeApiWrapper,
     KickAssAnimeApiWrapper,
     FzMoviesWrapper,
-    MobileTvShowsWrapper
+    MobileTvShowsWrapper,
 };
