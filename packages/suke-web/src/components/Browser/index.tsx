4d10c359d3dd54fa8e2875c2b9ed05f442efa8b8
khai93 <33293519+khai93@users.noreply.github.com>
2022-03-11 04:52:48 -0600

    add subtitle to fast32 source

--- packages/suke-web/src/components/Browser/index.tsx
+++ packages/suke-web/src/components/Browser/index.tsx
@@ -168,6 +168,7 @@ export const Browser = ({ setActive, roomId, requests, active }: BrowserProps) =
                 <SearchBar loading={loading} value={searchInput} setValue={setSearchInput} onSubmit={onSearchSubmit} size='full' placeholder={"Search " + (activeSource ? activeSource.charAt(0).toUpperCase() + activeSource.slice(1) : "Videos") + "..."} className={classNames(
                     'py-2',
                     'mx-auto ml-3 w-9/12 lg:mx-auto lg:w-7/12',
+                    "text-spaceblack",
                     'lg:ml-72'
                 )} rounded/>
                 <button>
