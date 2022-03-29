00900dc59baec8c2140f4a5c70cc61c5dfe23021
khai93 <33293519+khai93@users.noreply.github.com>
2022-03-29 05:36:17 -0500

    fix url in prod

--- packages/suke-web/src/util/apiUrl.ts
+++ packages/suke-web/src/util/apiUrl.ts
@@ -2,7 +2,7 @@ export const apiUrl = (path: string) => {
     if (process.env.NODE_ENV === 'production') {
         return new URL(path, 'http://localhost:4000')
     } else {
-        return new URL(path, 'http://localhost:4000')
+        return new URL(path, window.location.protocol + "//" + window.location.host);
     }
 }
 
